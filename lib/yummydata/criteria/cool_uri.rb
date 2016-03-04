82efb6a7851fb30d08692b38713593a17d060ace
takuodoi <doi@level-five.jp>
2016-03-04 21:01:08 +0900

    remove lower case criteria from Cool URI rule.

--- lib/yummydata/criteria/cool_uri.rb
+++ lib/yummydata/criteria/cool_uri.rb
@@ -3,11 +3,10 @@ module Yummydata
     module CoolURI
       def cool_uri_rate(uri)
         rate = 0
-        rate += 20 if uri.host !~ /\d+\.\d+\.\d+\.\d+/
-        rate += 20 if uri.to_s == uri.to_s.downcase
-        rate += 20 if uri.port == 80
-        rate += 20 if uri.query.nil?
-        rate += 20 if uri.to_s.length <= 30
+        rate += 25 if uri.host !~ /\d+\.\d+\.\d+\.\d+/
+        rate += 25 if uri.port == 80
+        rate += 25 if uri.query.nil?
+        rate += 25 if uri.to_s.length <= 30
         return rate
       end
     end
