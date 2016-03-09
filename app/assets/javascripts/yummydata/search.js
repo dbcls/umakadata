c71af4d455b1118874d9a4e4a7c69982be6ea3ca
Shunsuke-Komuta <komuta@level-five.jp>
2016-03-09 14:25:35 +0900

    Add anchors to the value of "NAME" and "URL", respectively

--- app/assets/javascripts/yummydata/search.js
+++ app/assets/javascripts/yummydata/search.js
@@ -8,10 +8,11 @@ $(function() {
     $.getJSON(url, function(data) {
       $("#result").show();
       for (var i = 0; i < data.length; i++) {
+        var endpoint = data[i];
         var row = $("<tr>");
-        row.append($("<td>").text(data[i].name));
-        row.append($("<td>").text(data[i].url));
-        row.append($("<td>").text(data[i].evaluation.score));
+        row.append($("<td>").append($("<a>").attr("href", "/endpoints/" + endpoint.id).text(endpoint.name)));
+        row.append($("<td>").append($("<a>").attr("href", endpoint.url).text(endpoint.url)));
+        row.append($("<td>").text(endpoint.evaluation.score));
         $("#result_body").append(row);
       }
     });
