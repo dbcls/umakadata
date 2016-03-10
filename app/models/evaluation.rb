067658428385499f31d809ce828e918f252cc0e8
takuodoi <doi@level-five.jp>
2016-03-10 22:50:02 +0900

    fix the name of stylesheet for tooltip.

--- app/models/evaluation.rb
+++ app/models/evaluation.rb
@@ -120,33 +120,33 @@ class Evaluation < ActiveRecord::Base
   end
 
   def self.calc_rates(eval)
-    rates = [0, 0, 0, 0, 0, 0]
+    rates = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
 
     # availability
     rates[0] += eval.alive_rate unless eval.alive_rate.blank?
 
     #freshness
-    rates[1] = 50
+    rates[1] = 50.0
 
     #operation
-    rates[2] += 50 unless eval.service_description.blank?
-    rates[2] += 50 unless eval.void_ttl.blank?
+    rates[2] += 50.0 unless eval.service_description.blank?
+    rates[2] += 50.0 unless eval.void_ttl.blank?
 
     #usefulness
-    rates[3] += 40 * (eval.vocabulary_score > 10 ? 10 : eval.vocabulary_score) / 10.0 unless eval.vocabulary_score.blank?
-    rates[3] += 30 * eval.ontology_score / 100.0 unless eval.ontology_score.blank?
-    rates[3] += 30 * eval.metadata_score / 100.0 unless eval.metadata_score.blank?
+    rates[3] += 40.0 * (eval.vocabulary_score > 10.0 ? 10.0 : eval.vocabulary_score) / 10.0 unless eval.vocabulary_score.blank?
+    rates[3] += 30.0 * eval.ontology_score / 100.0 unless eval.ontology_score.blank?
+    rates[3] += 30.0 * eval.metadata_score / 100.0 unless eval.metadata_score.blank?
 
     #validity
-    rates[4] += 40 * eval.cool_uri_rate.to_f / 100.0 unless eval.cool_uri_rate.blank?
-    rates[4] += 15 if eval.subject_is_uri
-    rates[4] += 15 if eval.subject_is_http_uri
-    rates[4] += 15 if eval.uri_provides_info
-    rates[4] += 15 if eval.contains_links
+    rates[4] += 40.0 * eval.cool_uri_rate.to_f / 100.0 unless eval.cool_uri_rate.blank?
+    rates[4] += 15.0 if eval.subject_is_uri
+    rates[4] += 15.0 if eval.subject_is_http_uri
+    rates[4] += 15.0 if eval.uri_provides_info
+    rates[4] += 15.0 if eval.contains_links
 
     #performance
-    rates[5] = 100 * (1.0 - eval.execution_time) unless eval.execution_time.blank?
-    rates[5] = 0 if rates[5] < 0
+    rates[5] = 100.0 * (1.0 - eval.execution_time) unless eval.execution_time.blank?
+    rates[5] = 0.0 if rates[5] < 0.0
 
     return rates.map{ |v| v.to_i }
   end
