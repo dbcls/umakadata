94376f8146f7b021c6b39e04545655752fe22489
masaaki konno <konno@leve-five.jp>
2016-03-16 19:35:09 +0900

    Add module that handles the error

---
+++ lib/yummydata/error_helper.rb
@@ -0,0 +1,18 @@
+module Yummydata
+  module ErrorHelper
+
+    def prepare
+      @errors = [] if @errors == nil
+    end
+
+    def set_error(key, value)
+      self.prepare
+      @errors[key] = value
+    end
+
+    def get_error(key)
+      @errors[key]
+    end
+
+  end
+end
