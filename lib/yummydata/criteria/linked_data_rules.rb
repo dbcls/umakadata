27256ebf1aa40101f0c7543eba167d2a615ef85f
Shunsuke-Komuta <komuta@level-five.jp>
2016-03-18 15:31:04 +0900

    add the function to set error reason to linked data rules

--- lib/yummydata/criteria/linked_data_rules.rb
+++ lib/yummydata/criteria/linked_data_rules.rb
@@ -1,10 +1,12 @@
 require 'yummydata/http_helper'
+require 'yummydata/error_helper'
 
 module Yummydata
   module Criteria
     module LinkedDataRules
 
       include Yummydata::HTTPHelper
+      include Yummydata::ErrorHelper
 
       def prepare(uri)
         @client = SPARQL::Client.new(uri, {'read_timeout': 5 * 60}) if @uri == uri && @client == nil
@@ -29,10 +31,11 @@ SPARQL
         begin
           results = @client.query(sparql_query)
         rescue => e
+          set_error(e)
           return false
         end
 
-        results != nil && results.count == 0
+        self.has_no_count?(results)
       end
 
       def http_subject?(uri)
@@ -53,9 +56,10 @@ SPARQL
         begin
           results = @client.query(sparql_query)
         rescue => e
+          set_error(e)
           return false
         end
-        results != nil && results.count == 0
+        self.has_no_count?(results)
       end
 
       def uri_provides_info?(uri)
@@ -68,10 +72,20 @@ SPARQL
         begin
           response = http_get_recursive(URI(uri), {}, 10)
         rescue => e
+          set_error("INVALID URI: #{uri}")
           puts "INVALID URI: #{uri}"
           return false
         end
-        response.is_a?(Net::HTTPSuccess) && !response.body.empty?
+
+        if !response.is_a?(Net::HTTPSucces)
+          set_error(http_response)
+        else
+          if !response.body.empty?
+            return true
+          end
+          set_error("html body is empty")
+        end
+        return false
       end
 
       def get_subject_randomly
@@ -90,11 +104,13 @@ SPARQL
         begin
           results = @client.query(sparql_query)
         rescue => e
+          set_error(e)
           return nil
         end
         if results != nil && results[0] != nil
           results[0][:s]
         else
+          set_error("sparql query has no result")
           nil
         end
       end
@@ -118,9 +134,11 @@ SPARQL
         begin
           results = @client.query(sparql_query)
         rescue => e
+          set_error(e)
           return false
         end
-        results != nil && results.count > 0
+
+        self.has_count?(results)
       end
 
       def contains_see_also?
@@ -136,9 +154,29 @@ SPARQL
         begin
           results = @client.query(sparql_query)
         rescue => e
+          set_error(e)
           return false
         end
-        results != nil && results.count > 0
+
+        self.has_count?(results)
+      end
+
+      def has_no_count?(results)
+          if results == nil
+            set_error("sparql query has no result")
+          else
+            return true if results.count == 0
+            set_error("the count of result is not 0")
+          end
+          return false
+      end
+
+      def has_count?(results)
+        if results != nil && results.count > 0
+          return true
+        end
+        set_error("sparql query has no result")
+        return false
       end
 
     end
