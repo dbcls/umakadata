992490e536f05fec79f320f431b7eca82eef1312
masaaki konno <konno@leve-five.jp>
2016-03-18 17:20:32 +0900

    Add error helper to cool uri module.

--- lib/yummydata/criteria/cool_uri.rb
+++ lib/yummydata/criteria/cool_uri.rb
@@ -1,12 +1,35 @@
+require 'yummydata/error_helper'
+
 module Yummydata
   module Criteria
     module CoolURI
+
+      include Yummydata::ErrorHelper
+
       def cool_uri_rate(uri)
+        error = []
         rate = 0
-        rate += 25 if uri.host !~ /\d+\.\d+\.\d+\.\d+/
-        rate += 25 if uri.port == 80
-        rate += 25 if uri.query.nil?
-        rate += 25 if uri.to_s.length <= 30
+        if uri.host !~ /\d+\.\d+\.\d+\.\d+/
+          rate += 25
+        else
+          error.push('Host is IP')
+        end
+        if uri.port == 80
+          rate += 25
+        else
+          error.push('Port is specified')
+        end
+        if uri.query.nil?
+          rate += 25
+        else
+          error.push('URL query is specified')
+        end
+        if uri.to_s.length <= 30
+          rate += 25
+        else
+          error.push('URI is longer than 30')
+        end
+        set_error(error.join(',')) if !error.empty?
         return rate
       end
     end
