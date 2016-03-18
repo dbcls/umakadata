06a66efd2a86a2839c62b015f8f7491ff5a376ef
masaaki konno <konno@leve-five.jp>
2016-03-18 17:40:12 +0900

    Add error helper to content negotiation module

--- lib/yummydata/criteria/content_negotiation.rb
+++ lib/yummydata/criteria/content_negotiation.rb
@@ -1,10 +1,12 @@
 require 'yummydata/http_helper'
+require 'yummydata/error_helper'
 
 module Yummydata
   module Criteria
     module ContentNegotiation
 
       include Yummydata::HTTPHelper
+      include Yummydata::ErrorHelper
 
       def check_content_negotiation(uri, content_type)
         query = <<-'SPARQL'
@@ -21,9 +23,18 @@ SPARQL
         request =  URI(uri.to_s + "?query=" + query)
 
         response = http_get_recursive(request, headers)
-        return false if response.nil?
+        if !response.is_a?(Net::HTTPSuccess)
+          set_error(response)
+          return false
+        end
 
-        return response.content_type == content_type
+        if response.content_type == content_type
+          return true
+        else
+          set_error('Content-Type is unexpected')
+          return false
+        end
+
       end
     end
   end
--- spec/yummydata/criteria/content_negotiation_spec.rb
+++ spec/yummydata/criteria/content_negotiation_spec.rb
@@ -17,54 +17,66 @@ describe 'Yummydata' do
           response = double(Net::HTTPResponse)
           allow(response).to receive(:content_type).and_return(Yummydata::DataFormat::TURTLE)
           allow(target).to receive(:http_get_recursive).and_return(response)
+          allow(response).to receive(:is_a?).and_return(true)
 
           result = target.check_content_negotiation(@uri, Yummydata::DataFormat::TURTLE)
           expect(result).to eq(true)
+          expect(target.get_error).to eq nil
         end
 
         it 'detect_not_ttl_support_if_response_is_not_ttl' do
           response = double(Net::HTTPResponse)
           allow(response).to receive(:content_type).and_return(Yummydata::DataFormat::RDFXML)
           allow(target).to receive(:http_get_recursive).and_return(response)
+          allow(response).to receive(:is_a?).and_return(true)
 
           result = target.check_content_negotiation(@uri, Yummydata::DataFormat::TURTLE)
           expect(result).to eq(false)
+          expect(target.get_error).to eq 'Content-Type is unexpected'
         end
 
         it 'detect_rdfxml_support_if_response_is_rdfxml' do
           response = double(Net::HTTPResponse)
           allow(response).to receive(:content_type).and_return(Yummydata::DataFormat::RDFXML)
           allow(target).to receive(:http_get_recursive).and_return(response)
+          allow(response).to receive(:is_a?).and_return(true)
 
           result = target.check_content_negotiation(@uri, Yummydata::DataFormat::RDFXML)
           expect(result).to eq(true)
+          expect(target.get_error).to eq nil
         end
 
         it 'detect_not_rdfxml_support_if_response_is_rdfxml' do
           response = double(Net::HTTPResponse)
           allow(response).to receive(:content_type).and_return(Yummydata::DataFormat::TURTLE)
           allow(target).to receive(:http_get_recursive).and_return(response)
+          allow(response).to receive(:is_a?).and_return(true)
 
           result = target.check_content_negotiation(@uri, Yummydata::DataFormat::RDFXML)
           expect(result).to eq(false)
+          expect(target.get_error).to eq 'Content-Type is unexpected'
         end
 
         it 'detect_html_support_if_response_is_html' do
           response = double(Net::HTTPResponse)
           allow(response).to receive(:content_type).and_return(Yummydata::DataFormat::HTML)
           allow(target).to receive(:http_get_recursive).and_return(response)
+          allow(response).to receive(:is_a?).and_return(true)
 
           result = target.check_content_negotiation(@uri, Yummydata::DataFormat::HTML)
           expect(result).to eq(true)
+          expect(target.get_error).to eq nil
         end
 
         it 'detect_not_html_support_if_response_is_turtle' do
           response = double(Net::HTTPResponse)
           allow(response).to receive(:content_type).and_return(Yummydata::DataFormat::TURTLE)
           allow(target).to receive(:http_get_recursive).and_return(response)
+          allow(response).to receive(:is_a?).and_return(true)
 
           result = target.check_content_negotiation(@uri, Yummydata::DataFormat::HTML)
           expect(result).to eq(false)
+          expect(target.get_error).to eq 'Content-Type is unexpected'
         end
 
       end
