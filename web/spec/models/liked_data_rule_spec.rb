427646d67f8a5dc0f9e1886bdd4946af0d178035
Daisuke Satoh <daisuke.satoh@level-five.jp>
2016-04-25 13:12:29 +0900

    fix typo

--- web/spec/models/liked_data_rule_spec.rb
+++ web/spec/models/liked_data_rule_spec.rb
@@ -1,5 +1,5 @@
 require 'rails_helper'
 
-RSpec.describe LikedDataRule, type: :model do
+RSpec.describe LinkedDataRule, type: :model do
   pending "add some examples to (or delete) #{__FILE__}"
 end
