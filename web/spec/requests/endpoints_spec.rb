0588921e1be5b5bf6f099491840e9919f1ae078f
Shunsuke-Komuta <komuta@level-five.jp>
2016-05-23 14:56:31 +0900

    enable to pass test for requests/endpoints_spec

--- web/spec/requests/endpoints_spec.rb
+++ web/spec/requests/endpoints_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe "Endpoints", type: :request do
   describe "GET /endpoints" do
     it "works! (now write some real specs)" do
-      get endpoints_path
+      get root_path
       expect(response).to have_http_status(200)
     end
   end
