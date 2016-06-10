0aeef44dd4416e396f20e99f86df5f3fc721bfe9
Shunsuke-Komuta <komuta@level-five.jp>
2016-06-10 11:38:05 +0900

    implement for nil check

--- web/app/models/endpoint.rb
+++ web/app/models/endpoint.rb
@@ -2,17 +2,17 @@ class Endpoint < ActiveRecord::Base
   has_many :evaluations
   has_one :evaluation
 
-  after_create do
-    issue = GithubHelper.create_issue(self.name)
-    self.update_column(:issue_id, issue[:number]) unless issue.nil?
-  end
-
-  after_update do
-    GithubHelper.edit_issue(self.issue_id, self.name)
+  after_save do
+    if self.issue_id.nil?
+      issue = GithubHelper.create_issue(self.name)
+      self.update_column(:issue_id, issue[:number]) unless issue.nil?
+    else
+      GithubHelper.edit_issue(self.issue_id, self.name)
+    end
   end
 
   after_destroy do
-    GithubHelper.close_issue(self.issue_id)
+    GithubHelper.close_issue(self.issue_id) unless self.issue_id.nil?
   end
 
 end
