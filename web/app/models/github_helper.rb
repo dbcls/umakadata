9d6d3538250ad07d9e15a7a50a75ce3868dc98f0
Shunsuke-Komuta <komuta@level-five.jp>
2016-07-26 10:49:14 +0900

    remove unnecessary temporary variable

--- web/app/models/github_helper.rb
+++ web/app/models/github_helper.rb
@@ -3,35 +3,30 @@ require 'octokit'
 class GithubHelper
 
   def self.create_issue(title)
-    proc = Proc.new {|client, github_repo| client.issues(github_repo)}
-    issues = call_github_api(proc)
+    issues = call_github_api {|client, github_repo| client.issues(github_repo)}
     return if issues.nil?
     dose_not_exist_issue = issues.select {|issue| issue[:title] == title}.empty?
     if dose_not_exist_issue
-      proc = Proc.new{|client, github_repo| client.create_issue(github_repo, title)}
-      call_github_api(proc)
+      call_github_api {|client, github_repo| client.create_issue(github_repo, title)}
     end
   end
 
   def self.edit_issue(number, title)
-    proc = Proc.new{|client, github_repo| client.update_issue(github_repo, number, title)}
-    call_github_api(proc)
+    call_github_api {|client, github_repo| client.update_issue(github_repo, number, title)}
   end
 
   def self.close_issue(number)
-    proc = Proc.new{|client, github_repo| client.close_issue(github_repo, number)}
-    call_github_api(proc)
+    call_github_api {|client, github_repo| client.close_issue(github_repo, number)}
   end
 
   def self.list_issues
-    proc = Proc.new{|client, github_repo|
+    call_github_api do |client, github_repo|
       client.auto_paginate = true
       client.list_issues(github_repo, {:state => 'all'})
-    }
-    call_github_api(proc)
+    end
   end
 
-  def self.call_github_api(proc)
+  def self.call_github_api
     if Rails.application.secrets.github_token.blank? || Rails.application.secrets.github_repo.blank?
       p "Does not set github api configuration"
       return
@@ -39,7 +34,7 @@ class GithubHelper
 
     begin
       client = Octokit::Client.new(:access_token => Rails.application.secrets.github_token)
-      return proc.call(client, Rails.application.secrets.github_repo)
+      yield(client, Rails.application.secrets.github_repo)
     rescue => e
       p e.message
     end
