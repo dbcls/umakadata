329a822bfd39b5c5c809383559dcfcc48499bf6a
masaaki konno <konno@level-five.jp>
2016-09-12 10:31:51 +0900

    Create created_at index for endpoint

--- web/db/schema.rb
+++ web/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160824100030) do
+ActiveRecord::Schema.define(version: 20160909065743) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -111,6 +111,8 @@ ActiveRecord::Schema.define(version: 20160824100030) do
     t.text     "license"
     t.text     "publisher"
   end
+
+  add_index "evaluations", ["created_at"], name: "index_evaluations_on_created_at", using: :btree
 
   create_table "prefix_filters", force: :cascade do |t|
     t.integer  "endpoint_id"
