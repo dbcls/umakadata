e4ea5f90e68e4228890f92e3deb834f6922c1812
masaaki konno <konno@level-five.jp>
2016-09-29 17:02:31 +0900

    Modify the way of scoring ontologies

--- web/app/models/evaluation.rb
+++ web/app/models/evaluation.rb
@@ -88,9 +88,23 @@ class Evaluation < ActiveRecord::Base
       eval.metadata_log = logger.as_json
 
       unless metadata.empty?
+        list_of_ontologies_log = Umakadata::Logging::Log.new
+        list_of_ontologies = retriever.list_ontologies(metadata, logger: list_of_ontologies_log)
+
+        score_ontologies_for_endpoints_log = Umakadata::Logging::Log.new
+        score_ontologies_for_endpoints_log.push list_of_ontologies_log
         logger = Umakadata::Logging::Log.new
-        eval.ontology_score = retriever.score_ontologies(metadata, logger: logger)
+        logger.push score_ontologies_for_endpoints_log
+
+        score = retriever.score_ontologies_for_endpoints(list_of_ontologies, rdf_prefixes, logger: score_ontologies_for_endpoints_log)
+        logger.result = "Ontology score is #{score}"
+        eval.ontology_score = score
         eval.ontology_log = logger.as_json
+        RdfPrefix.delete_all(endpoint_id: endpoint.id)
+        list_of_ontologies.each do |ontology|
+          rdf_prefix = RdfPrefix.new(endpoint_id: endpoint.id, uri: ontology)
+          rdf_prefix.save
+        end
       end
 
       logger = Umakadata::Logging::Log.new
