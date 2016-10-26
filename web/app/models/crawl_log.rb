4a590c2f8cd7ed7406090d0f6f5c49f8d7b2a9e9
Shunsuke-Komuta <komuta@level-five.jp>
2016-10-26 11:12:56 +0900

    add scope to crawl_log model

--- web/app/models/crawl_log.rb
+++ web/app/models/crawl_log.rb
@@ -1,3 +1,5 @@
 class CrawlLog < ActiveRecord::Base
-    has_many :evaluations
+  has_many :evaluations
+  scope :latest, -> { where.not(finished_at: nil).last }
+  scope :started_at, ->(date) { where(started_at: date.all_day).take }
 end
