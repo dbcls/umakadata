0dc7eac1a44f054e45d034b366364b01cf379f84
masaaki konno <konno@level-five.jp>
2016-11-07 18:42:17 +0900

    Remove comment out

--- web/app/admin/dashboard.rb
+++ web/app/admin/dashboard.rb
@@ -7,8 +7,6 @@ ActiveAdmin.register_page "Dashboard" do
     div class: "blank_slate_container", id: "dashboard_default_message" do
       span class: "blank_slate" do
         link_to I18n.t("top_page"), "/"
-        # span I18n.t("active_admin.dashboard_welcome.welcome")
-        # small I18n.t("active_admin.dashboard_welcome.call_to_action")
       end
     end
 
