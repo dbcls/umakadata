5e3ee3aaea5c60dbb4436e8626fa9548c619bd97
Shunsuke-Komuta <komuta@level-five.jp>
2017-03-08 18:53:47 +0900

    add the validation of environment variable

--- web/script/config.sh
+++ web/script/config.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-readonly PROJECT_DIR="/sbMeta"
+if [ -z "$PROJECT_DIR" ]; then
+    echo "Please set PROJECT_DIR"
+    exit 1
+fi
+
+readonly PROJECT_DIR="$PROJECT_DIR"
 readonly DATA_DIR="$PROJECT_DIR/data"
 readonly CSV_FILE_PATH="$DATA_DIR/endpoints.csv"
 readonly ALL_PREFIX_CSV_PATH="$DATA_DIR/all_prefixes.csv"
