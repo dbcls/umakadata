39e8c47e789c699c2d4e6e9d45594be217837f7d
takuodoi <doi@level-five.jp>
2017-05-16 12:49:20 +0900

    Adjust graph node size in order to avoid node overlapping.

--- web/app/assets/javascripts/umakadata/graph.js
+++ web/app/assets/javascripts/umakadata/graph.js
@@ -1,42 +1,44 @@
-$(function() {
-  $("#cy").height(window.innerHeight - 150);
-  var url = "/api/endpoints/graph/"
-  $.getJSON(url, function(data) {
-    var cy = window.cy = cytoscape({
-      container: document.getElementById('cy'),
+$(function () {
+    $("#cy").height(window.innerHeight - 150);
+    var url = "/api/endpoints/graph/"
+    $.getJSON(url, function (data) {
+        var cy = window.cy = cytoscape({
+            container: document.getElementById('cy'),
 
-      layout: {
-        name: 'cose',
-        idealEdgeLength: 100,
-        nodeOverlap: 20
-      },
+            boxSelectionEnabled: false,
+            autounselectify: true,
 
-      style: [
-        {
-          selector: 'node',
-          style: {
-            content: "data(name)"
-          }
-        },
-        {
-          selector: 'edge',
-          style: {
-            'width': 3,
-            'line-color': '#ccc'
-          }
-        },
-        {
-          selector: 'edge',
-          css: {
-            'target-arrow-shape': 'triangle',
-            'curve-style': 'bezier'
-          }
-        }
-      ],
+            layout: {
+                name: 'cose',
+                nodeOverlap: 400,
+                fit: true,
+            },
 
-      elements: data
+            style: [
+                {
+                    selector: 'node',
+                    css: {
+                        'content': "data(name)",
+                        'height': '200',
+                        'width': '200',
+                        'text-valign': 'center',
+                        'text-halign': 'center'
+                    }
+                },
+                {
+                    selector: 'edge',
+                    css: {
+                        'width': 3,
+                        'line-color': '#ccc',
+                        'target-arrow-shape': 'triangle',
+                        'curve-style': 'bezier'
+                    }
+                }
+            ],
 
+            elements: data
+
+        });
     });
-  });
 
 });
