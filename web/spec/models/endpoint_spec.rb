5e5d906c94ea82bf44645419ffac5cb0011fb76f
Takayuki-Yamamoto <yamamoto@level-five.jp>
2018-07-10 12:09:33 +0900

    Fix test case and remove redundant test

--- web/spec/models/endpoint_spec.rb
+++ web/spec/models/endpoint_spec.rb
@@ -11,14 +11,14 @@ RSpec.describe Endpoint, type: :model do
 
   describe '::create_issue' do
 
-    it 'should return false if GithubHelper.create_issue() is failed' do
+    it 'should return false if issue already exists' do
       endpoint = Endpoint.create(:name => 'Endpoint 1', :url => 'http://www.example.com/sparql')
 
       allow(GithubHelper).to receive(:create_issue).and_return(nil)
       expect(Endpoint.create_issue(endpoint)).to eq(false)
     end
 
-    it 'should return false if GithubHelper.add_label() is failed' do
+    it 'should return false if label already exists' do
       endpoint = Endpoint.create(:name => 'Endpoint 1', :url => 'http://www.example.com/sparql')
       issue    = double(Sawyer::Resource)
 
@@ -29,23 +29,7 @@ RSpec.describe Endpoint, type: :model do
       expect(Endpoint.create_issue(endpoint)).to eq(false)
     end
 
-    it 'should return false if GithubHelper.add_labels_to_an_issue is failed' do
-      endpoint = Endpoint.create(:name => 'Endpoint 1', :url => 'http://www.example.com/sparql')
-      issue    = double(Sawyer::Resource)
-      label    = double(Sawyer::Resource)
-      results  = double([double(Sawyer::Resource)])
-
-      allow(issue).to receive(:[]).with(:number).and_return(1)
-      allow(label).to receive(:[]).with(:name).and_return('Endpoint 1')
-      allow(label).to receive(:[]).with(:id).and_return(1)
-      allow(GithubHelper).to receive(:create_issue).and_return(issue)
-      allow(GithubHelper).to receive(:add_label).and_return(label)
-      allow(GithubHelper).to receive(:add_labels_to_an_issue).and_return(nil)
-
-      expect(Endpoint.create_issue(endpoint)).to eq(false)
-    end
-
-    it 'should return true if Endpoint.create_issue() is Success' do
+    it 'should return true if issue and label do not yet exist' do
       endpoint = Endpoint.create(:name => 'Endpoint 1', :url => 'http://www.example.com/sparql')
       issue    = double(Sawyer::Resource)
       label    = double(Sawyer::Resource)
