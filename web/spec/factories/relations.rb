ba39c230fb9666a468e60fa32b578ce43ad91c21
Shota Matsumoto <shota.matsumoto@level-five.jp>
2018-07-11 18:08:01 +0900

    modify web/spec/factories/relations.rb

--- web/spec/factories/relations.rb
+++ web/spec/factories/relations.rb
@@ -1,8 +1,7 @@
 FactoryBot.define do
   factory :relation do
-    sequence :endpoint_id { |n| n }
-
-    sequence :dst_id { |n| n + 1 }
+    sequence(:endpoint_id){ |n| n }
+    sequence(:dst_id){ |n| n + 1 }
 
     name {["seeAlso","sameAs"].sample}
   end
