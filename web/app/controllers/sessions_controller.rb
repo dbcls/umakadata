ae53cae11afef19e4ac3d37b60fbaa8f09d0fa3d
Shota Matsumoto <shota.matsumoto@level-five.jp>
2018-07-24 17:46:50 +0900

    remove unused info from session

--- web/app/controllers/sessions_controller.rb
+++ web/app/controllers/sessions_controller.rb
@@ -2,9 +2,6 @@ class SessionsController < ApplicationController
   def callback
     auth                         = request.env['omniauth.auth']
     session[:oauth_token]        = auth.credentials.token
-    session[:oauth_token_secret] = auth.credentials.secret
-    session[:username]           = auth["info"]["nickname"]
-    session[:code]               = request.env['rack.request.query_hash']['code']
 
     redirect_to endpoints_after_authorization_path
   end
