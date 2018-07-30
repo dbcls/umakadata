c8652ae04c16ac578afa5e33607515d74d742220
Shota Matsumoto <shota.matsumoto@level-five.jp>
2018-07-30 16:22:40 +0900

    add migration  add_denied_and_case_sensitive_to_prefix.rb

--- web/db/schema.rb
+++ web/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180724063937) do
+ActiveRecord::Schema.define(version: 20180730071309) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -138,8 +138,10 @@ ActiveRecord::Schema.define(version: 20180724063937) do
   create_table "prefixes", force: :cascade do |t|
     t.integer  "endpoint_id"
     t.string   "uri"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",                    null: false
+    t.datetime "updated_at",                    null: false
+    t.string   "denied_uri"
+    t.boolean  "case_sensitive", default: true, null: false
   end
 
   create_table "rdf_prefixes", force: :cascade do |t|
