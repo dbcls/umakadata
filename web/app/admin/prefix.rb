d7517c1c4c7cf34e2fd3554ac1cd6712406ff7d8
Shota Matsumoto <shota.matsumoto@level-five.jp>
2018-07-30 16:33:10 +0900

    add new columns of prefixes in active admin

--- web/app/admin/prefix.rb
+++ web/app/admin/prefix.rb
@@ -7,6 +7,8 @@ ActiveAdmin.register Prefix do
     id_column
     column :endpoint
     column :uri
+    column :denied_uri
+    column :case_sensitive
     column :created_at
     column :updated_at
     actions
@@ -16,6 +18,8 @@ ActiveAdmin.register Prefix do
     column :id
     column :endpoint_id
     column :uri
+    column :denied_uri
+    column :case_sensitive
     column :created_at
     column :updated_at
   end
