bbcf6a12f9bd7bcfc435b51e0ef7f6b00b705f19
Takayuki-Yamamoto <yamamoto@lifematics.co.jp>
2018-08-31 14:34:18 +0900

    Change crawl_log_id to created_at

--- web/app/controllers/endpoints_controller.rb
+++ web/app/controllers/endpoints_controller.rb
@@ -435,16 +435,16 @@ class EndpointsController < ApplicationController
       metrics[:no_of_endpoints][:count]     = Endpoint.where('disable_crawling = ? AND created_at < ?', false, crawllog_today.finished_at).count
       metrics[:no_of_endpoints][:variation] = metrics[:no_of_endpoints][:count] - number_of_endpoints_last_week
 
-      active_endpoints_yesterday             = Evaluation.where(created_at: crawllog_yesterday.started_at..crawllog_yesterday.finished_at, alive: true).count(:endpoint_id)
-      metrics[:active_endpoints][:count]     = Evaluation.where(created_at: crawllog_today.started_at..crawllog_today.finished_at, alive: true).count(:endpoint_id)
+      active_endpoints_yesterday             = Evaluation.where(crawl_log_id: crawllog_yesterday.id, alive: true).count(:endpoint_id)
+      metrics[:active_endpoints][:count]     = Evaluation.where(crawl_log_id: crawllog_today.id, alive: true).count(:endpoint_id)
       metrics[:active_endpoints][:variation] = metrics[:active_endpoints][:count] - active_endpoints_yesterday
 
-      active_endpoints_last_week        = Evaluation.where(created_at: crawllog_last_week.started_at..crawllog_last_week.finished_at, alive: true).count(:endpoint_id)
+      active_endpoints_last_week        = Evaluation.where(crawl_log_id: crawllog_last_week.id, alive: true).count(:endpoint_id)
       metrics[:alive_rates][:count]     = ((metrics[:active_endpoints][:count].to_f / metrics[:no_of_endpoints][:count].to_f) * 100).round(0)
       metrics[:alive_rates][:variation] = metrics[:alive_rates][:count] - ((active_endpoints_last_week.to_f / number_of_endpoints_last_week.to_f) * 100).round(0) if number_of_endpoints_last_week > 0
 
-      data_entries_yesterday             = Evaluation.where(created_at: crawllog_yesterday.started_at..crawllog_yesterday.finished_at).sum(:number_of_statements)
-      metrics[:data_entries][:count]     = Evaluation.where(created_at: crawllog_today.started_at..crawllog_today.finished_at).sum(:number_of_statements)
+      data_entries_yesterday             = Evaluation.where(crawl_log_id: crawllog_yesterday.id).sum(:number_of_statements)
+      metrics[:data_entries][:count]     = Evaluation.where(crawl_log_id: crawllog_today.id).sum(:number_of_statements)
       metrics[:data_entries][:variation] = metrics[:data_entries][:count] - data_entries_yesterday
 
       metrics
