f250a30468e82148dc70c049161df6ebd6915354
Daisuke Satoh <daisuke.satoh@lifematics.co.jp>
2019-09-04 19:57:47 +0900

    initial import

---
+++ app/admin/resource_uris.rb
@@ -0,0 +1,94 @@
+ActiveAdmin.register ResourceURI do
+  permit_params :uri, :as_regex, :allow, :deny, :case_insensitive, :endpoint_id
+
+  config.sort_order = 'id_asc'
+
+  index do
+    selectable_column
+    id_column
+    column :endpoint
+    column 'URI', :uri
+    column :as_regex
+    column :allow
+    column :deny
+    column :case_insensitive
+    actions
+  end
+
+  filter :endpoint
+  filter :uri
+  filter :as_regex
+  filter :allow
+  filter :deny
+  filter :case_insensitive
+
+  show do
+    attributes_table do
+      row :endpoint
+      row :uri
+      row :as_regex
+      row :allow
+      row :deny
+      row :case_insensitive
+      row :created_at
+      row :updated_at
+    end
+    active_admin_comments
+  end
+
+  csv do
+    column :id
+    column :endpoint_id
+    column :uri
+    column :as_regex
+    column :allow
+    column :deny
+    column :case_insensitive
+    column :created_at
+    column :updated_at
+  end
+
+  form do |f|
+    f.semantic_errors
+    f.inputs do
+      input :endpoint
+      input :uri, label: 'URI'
+      input :as_regex
+      input :allow
+      input :deny
+      input :case_insensitive
+    end
+    f.actions do
+      add_create_another_checkbox
+      action(:submit, label: 'Add Prefix')
+      cancel_link
+    end
+    script do
+      raw <<-SCRIPT
+        $(function() {
+          let update_form = function() {
+            let as_regex = $('#resource_uri_as_regex').prop('checked');
+            $('#resource_uri_allow').prop('disabled', !as_regex);
+            $('#resource_uri_deny').prop('disabled', !as_regex);
+            $('#resource_uri_case_insensitive').prop('disabled', !as_regex);
+          };
+          update_form();
+          $('#resource_uri_as_regex').on('change', function() {
+            update_form();
+          });
+        });
+      SCRIPT
+    end
+  end
+
+  controller do
+    def create
+      super do |success, _|
+        success.html do
+          flash[:notice] = 'Prefix was successfully added.'
+          redirect_to admin_resource_uri_path(resource)
+        end
+      end
+    end
+  end
+end
