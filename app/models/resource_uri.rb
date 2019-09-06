2260e258f905a2dfb8f9216c1b46a42eab244849
Daisuke Satoh <daisuke.satoh@lifematics.co.jp>
2019-09-06 14:41:08 +0900

    add validation

--- app/models/resource_uri.rb
+++ app/models/resource_uri.rb
@@ -1,3 +1,13 @@
 class ResourceURI < ApplicationRecord
   belongs_to :endpoint
+
+  validate :both_uri_and_filter_cannot_be_specified
+
+  private
+
+  def both_uri_and_filter_cannot_be_specified
+    if uri.present? && (allow.present? || deny.present?)
+      errors.add(:base, 'Both URI and filters cannot be specified on a record.')
+    end
+  end
 end
