50e55113a192f7a1fdd04dbae13ec0e8d04b8e30
Daisuke Satoh <daisuke.satoh@lifematics.co.jp>
2019-09-06 16:39:26 +0900

    initial import

---
+++ app/admin/dataset_relations.rb
@@ -0,0 +1,63 @@
+ActiveAdmin.register DatasetRelation do
+  permit_params :src_endpoint_id, :dst_endpoint_id, :relation, :endpoint_id
+
+  config.sort_order = 'id_asc'
+
+  endpoints = Endpoint.all.order(:name).pluck(:name, :id).to_h
+
+  index do
+    selectable_column
+    id_column
+    column :endpoint
+    column :src_endpoint do |x|
+      link_to Endpoint.find(x.src_endpoint_id).name, admin_endpoint_path(x.src_endpoint_id)
+    end
+    column :dst_endpoint do |x|
+      link_to Endpoint.find(x.dst_endpoint_id).name, admin_endpoint_path(x.dst_endpoint_id)
+    end
+
+    actions
+  end
+
+  filter :endpoint
+  filter :src_endpoint_id, label: 'src endpoint', as: :select, collection: endpoints
+  filter :dst_endpoint_id, label: 'dst endpoint', as: :select, collection: endpoints
+
+  show do
+    attributes_table do
+      row :endpoint
+      row :src_endpoint do |x|
+        link_to Endpoint.find(x.src_endpoint_id).name, admin_endpoint_path(x.src_endpoint_id)
+      end
+      row :dst_endpoint do |x|
+        link_to Endpoint.find(x.dst_endpoint_id).name, admin_endpoint_path(x.dst_endpoint_id)
+      end
+    end
+    active_admin_comments
+  end
+
+  form do |f|
+    f.semantic_errors
+    f.inputs do
+      input :endpoint
+      input :src_endpoint_id, as: :select, collection: endpoints, include_blank: false
+      input :dst_endpoint_id, as: :select, collection: endpoints, include_blank: false
+    end
+    f.actions do
+      add_create_another_checkbox
+      action(:submit, label: 'Add Dataset Relation')
+      cancel_link
+    end
+  end
+
+  controller do
+    def create
+      super do |success, _|
+        success.html do
+          flash[:notice] = 'Dataset Relation was successfully added.'
+          redirect_to admin_dataset_relation_path(resource)
+        end
+      end
+    end
+  end
+end
