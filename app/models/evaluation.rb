8294b5e920e85ce31a6c7611b5ca9a29ff2b3aa6
Daisuke Satoh <daisuke.satoh@lifematics.co.jp>
2019-09-09 14:31:10 +0900

    WIP: add callback to calculate score for evaluation

--- app/models/evaluation.rb
+++ app/models/evaluation.rb
@@ -4,4 +4,11 @@ class Evaluation < ApplicationRecord
   has_many :measurements, dependent: :destroy
 
   attribute :exceptions, :binary_hash
+
+  before_save :evaluate_measurements
+
+  def evaluate_measurements
+    return unless measurements.any? { |x| x.new_record? || x.changed? }
+    # TODO: IMPLEMENT ME!
+  end
 end
