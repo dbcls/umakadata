4a024e6f2c1a0b38abdb90c258589f4529ed2c94
Daisuke Satoh <daisuke.satoh@lifematics.co.jp>
2019-09-10 13:51:21 +0900

    initial import

---
+++ app/jobs/runner_job.rb
@@ -0,0 +1,17 @@
+class RunnerJob < ActiveJob::Base
+  sidekiq_options queue: :runner
+
+  def perform
+    Crawl.start! if scheduled_time? && crawl_not_performed?
+  end
+
+  private
+
+  def scheduled_time?
+    Crawl.start_time(Date.current) <= Time.current
+  end
+
+  def crawl_not_performed?
+    Crawl.find_by(started_at: Date.current.all_day).blank?
+  end
+end
