d72d03c09f4e2a6d91c91c73fd7ad3973cf05f5e
Daisuke Satoh <daisuke.satoh@lifematics.co.jp>
2019-09-10 14:41:53 +0900

    mount Sidekiq::Web

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,18 @@
+require 'sidekiq/web'
+require 'sidekiq-scheduler/web'
+
 Rails.application.routes.draw do
+  root 'root#index'
+
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
-  root 'root#index'
+
+  if ENV['SIDEKIQ_USER'].present? && ENV['SIDEKIQ_PASSWORD'].present?
+    Sidekiq::Web.use Rack::Auth::Basic do |username, password|
+      ActiveSupport::SecurityUtils.secure_compare(::Digest::SHA256.hexdigest(username), ::Digest::SHA256.hexdigest(ENV['SIDEKIQ_USER'])) &
+        ActiveSupport::SecurityUtils.secure_compare(::Digest::SHA256.hexdigest(password), ::Digest::SHA256.hexdigest(ENV['SIDEKIQ_PASSWORD']))
+    end
+  end
+
+  mount Sidekiq::Web, at: '/sidekiq'
 end
