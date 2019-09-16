e43af4809c7b0e04ab55e5336ca7a323aecd6e84
Daisuke Satoh <daisuke.satoh@lifematics.co.jp>
2019-09-16 13:02:30 +0900

    add config for google analytics

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,4 +90,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # Set Google Analytics tracker code
+  GA.tracker = ENV['UMAKADATA_GA_TRACKER']
 end
