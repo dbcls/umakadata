6776429a3d2dd22161855fe014dbb530b6ef22c9
Daisuke Satoh <daisuke.satoh@lifematics.co.jp>
2019-09-16 20:21:51 +0900

    add config for jQuery

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,4 +1,22 @@
-const { environment } = require('@rails/webpacker');
+const {environment} = require('@rails/webpacker');
+const webpack = require('webpack');
+
+environment.plugins.prepend('Provide',
+  new webpack.ProvidePlugin({
+    $: 'jquery/dist/jquery',
+    jQuery: 'jquery/dist/jquery',
+    Popper: 'popper.js/dist/popper'
+  })
+);
+
+// To enable jQuery for $ in browser console
+environment.loaders.append('expose', {
+  test: require.resolve('jquery'),
+  use: [{
+    loader: 'expose-loader',
+    options: '$'
+  }]
+});
 
 const erb = require('./loaders/erb');
 environment.loaders.prepend('erb', erb);
