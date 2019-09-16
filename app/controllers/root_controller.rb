842ef4a00a21fc0d4a669d6fdacd3217a766123d
Daisuke Satoh <daisuke.satoh@lifematics.co.jp>
2019-09-16 20:27:58 +0900

    implement functions for dashboard

--- app/controllers/root_controller.rb
+++ app/controllers/root_controller.rb
@@ -1,4 +1,13 @@
 class RootController < ApplicationController
+  before_action :set_current_date, :dashboard
+  before_action :set_start_date, :dashboard
+
+  # GET /dashboard
+  def dashboard
+    crawl = Crawl.where(started_at: @current_date.all_day).first
+    @evaluations = crawl.evaluations.order(score: :desc).limit(5)
+  end
+
   # GET /inquiry
   def inquiry
     @inquiry = Inquiry.new
@@ -24,7 +33,25 @@ class RootController < ApplicationController
 
   private
 
+  def dashboard_params
+    params.permit(:date)
+  end
+
   def inquiry_params
     params.require(:inquiry).permit(:name, :email, :message)
+  end
+
+  def set_current_date
+    @current_date = if dashboard_params[:date].present?
+              Date.parse(dashboard_params[:date])
+            else
+              xs = Crawl.order(started_at: :desc).limit(2).to_a
+              xs.shift unless xs.first.finished?
+              xs.first.started_at.to_date
+            end
+  end
+
+  def set_start_date
+    @start_date = Crawl.order(:started_at).first.started_at.to_date
   end
 end
