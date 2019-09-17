0bb892433539b438b0433bce48e648a2fc1ebc6e
Daisuke Satoh <daisuke.satoh@lifematics.co.jp>
2019-09-17 16:50:34 +0900

    add config for action mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,6 +67,11 @@ Rails.application.configure do
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_url_options = { host: ENV['UMAKADATA_MAILER_DEFAULT_URL_HOST'] }
+  config.action_mailer.default_options = { to: ENV['UMAKADATA_MAILER_DEFAULT_TO'] }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = { address: ENV['UMAKADATA_MAILER_SMTP_ADDRESS'] }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
