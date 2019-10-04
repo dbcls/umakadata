0395135d36a32c5046e18fdd5224e342c6b35324
Daisuke Satoh <daisuke.satoh@lifematics.co.jp>
2019-10-05 07:55:58 +0900

    refactor

--- config/initializers/sidekiq_scheduler.rb
+++ config/initializers/sidekiq_scheduler.rb
@@ -2,12 +2,14 @@ require 'sidekiq'
 require 'sidekiq-scheduler'
 
 Sidekiq.configure_server do |config|
-  config.on(:startup) do
-    Sidekiq.schedule = Rails.env.production? ? YAML.load_file(Rails.root.join('config', 'sidekiq_scheduler.yml')) : {}
-    SidekiqScheduler::Scheduler.instance.reload_schedule!
+  if Rails.env.production? && ENV['UMAKADATA_DISABLE_CRAWLING'].blank?
+    config.on(:startup) do
+      Sidekiq.schedule = YAML.load_file(Rails.root.join('config', 'sidekiq_scheduler.yml'))
+      SidekiqScheduler::Scheduler.instance.reload_schedule!
+    end
+  else
+    SidekiqScheduler::Scheduler.instance.enabled = false
   end
-
-  SidekiqScheduler::Scheduler.instance.enabled = false if Rails.env.development?
 
   Rails.logger.info "SidekiqScheduler is #{SidekiqScheduler::Scheduler.instance.enabled ? 'enabled' : 'disabled'}."
 end
