220aa06ed51f4e0dfbb954be336a6e1ee168c2c1
Daisuke Satoh <daisuke.satoh@lifematics.co.jp>
2019-10-27 15:16:39 +0900

    add crawl status on dash board

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -1,33 +1,72 @@
 ActiveAdmin.register_page "Dashboard" do
+  menu priority: 1
 
-  menu priority: 1, label: proc{ I18n.t("active_admin.dashboard") }
-
-  content title: proc{ I18n.t("active_admin.dashboard") } do
-    div class: "blank_slate_container", id: "dashboard_default_message" do
-      span class: "blank_slate" do
-        span I18n.t("active_admin.dashboard_welcome.welcome")
-        small I18n.t("active_admin.dashboard_welcome.call_to_action")
+  content do
+    panel 'Current crawling' do
+      if (jobs = controller.instance_variable_get(:@processing)).present?
+        jobs.each do |job|
+          attributes_table_for job do
+            row :started_at
+            row :processing do
+              ul do
+                job[:processing].each do |id, name|
+                  li do
+                    status_tag id, class: 'no'
+                    span name
+                  end
+                end
+              end
+              nil
+            end
+            row :finished
+            row :total
+          end
+        end
+      else
+        div class: 'no_jobs' do
+          span 'No crawling in progress'
+        end
       end
     end
 
-    # Here is an example of a simple dashboard with columns and panels.
-    #
-    # columns do
-    #   column do
-    #     panel "Recent Posts" do
-    #       ul do
-    #         Post.recent(5).map do |post|
-    #           li link_to(post.title, admin_post_path(post))
-    #         end
-    #       end
-    #     end
-    #   end
+    panel 'Scheduled crawlings' do
+      if (jobs = controller.instance_variable_get(:@scheduled)).present?
+        table_for jobs do
+          column :start_at
+          column 'endpoint order', :order
+        end
+      else
+        div class: 'no_jobs' do
+          span 'No scheduled crawlings'
+        end
+      end
+    end
+  end
 
-    #   column do
-    #     panel "Info" do
-    #       para "Welcome to ActiveAdmin."
-    #     end
-    #   end
-    # end
-  end # content
+  controller do
+    def index
+      total = Endpoint.active.count
+
+      @processing = if (xs = Crawl.processing).present?
+                      xs.processing.map do |x|
+                        {
+                          started_at: x.started_at.to_formatted_s,
+                          processing: Endpoint.where(id: x.processing.map(&:last)).order(:id).pluck(:id, :name),
+                          finished: x.evaluations.count - (x.processing.size),
+                          total: total
+                        }
+                      end
+                    else
+                      []
+                    end
+
+      @scheduled = (Time.current < Crawl.start_time(Date.current) ? (0..7) : (1..8)).map do |n|
+        date = Date.current.since(n.days)
+        {
+          start_at: Crawl.start_time(date).to_formatted_s,
+          order: Crawl.queue_order(date)
+        }
+      end
+    end
+  end
 end
