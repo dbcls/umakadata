e7f9b42ca0931a3bf31796d6f6c56b927387b12e
Daisuke Satoh <daisuke.satoh@lifematics.co.jp>
2020-02-03 13:56:09 +0900

    add notification field

--- app/admin/admin_users.rb
+++ app/admin/admin_users.rb
@@ -1,5 +1,5 @@
 ActiveAdmin.register AdminUser do
-  permit_params :email, :password, :password_confirmation
+  permit_params :email, :password, :password_confirmation, :notification
 
   config.sort_order = 'id_asc'
 
@@ -7,6 +7,7 @@ ActiveAdmin.register AdminUser do
     selectable_column
     id_column
     column :email
+    column :notification
     column :current_sign_in_at
     column :sign_in_count
     column :created_at
@@ -21,6 +22,7 @@ ActiveAdmin.register AdminUser do
   form do |f|
     f.inputs do
       f.input :email
+      f.input :notification
       f.input :password
       f.input :password_confirmation
     end
