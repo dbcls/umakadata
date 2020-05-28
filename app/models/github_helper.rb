a5584b3c6f6f46d3b6eaf3b22bb8f1012e5f4d6c
Daisuke Satoh <daisuke.satoh@lifematics.co.jp>
2020-05-28 16:35:26 +0900

    just reorder by destructive API or not

--- app/models/github_helper.rb
+++ app/models/github_helper.rb
@@ -1,16 +1,46 @@
 require 'octokit'
 
 class GithubHelper
+  #
+  # non-destructive API
+  #
+
+  def self.list_issues(options = {})
+    call_github_api do |client, github_repo|
+      client.auto_paginate = true
+      client.list_issues(github_repo, **options)
+    end
+  end
+
+  def self.labels_for_issue(number, options = {})
+    call_github_api { |client, github_repo| client.labels_for_issue(github_repo, number, options) }
+  end
+
+  def self.get_label(name)
+    call_github_api { |client, github_repo| client.label(github_repo, name) }
+  end
+
+  def self.label_exists?(name)
+    # idempotent API
+    labels = call_github_api { |client, github_repo| client.labels(github_repo) }
+    !labels.select { |label| label[:name] == name }.empty?
+  end
+
+  def self.issue_exists?(title)
+    issues = call_github_api { |client, github_repo| client.issues(github_repo) }
+    !issues.select { |issue| issue[:title] == title }.empty?
+  end
+
+  #
+  # destructive API
+  #
+
   def self.create_issue(title, body = nil, options = {})
     call_github_api { |client, github_repo| client.create_issue(github_repo, title, body, options) }
   end
 
   def self.update_issue(number, title)
     call_github_api { |client, github_repo| client.update_issue(github_repo, number, title) }
-  end
-
-  def self.labels_for_issue(number, options = {})
-    call_github_api { |client, github_repo| client.labels_for_issue(github_repo, number, options) }
   end
 
   def self.add_labels_to_an_issue(number, labels)
@@ -21,19 +51,8 @@ class GithubHelper
     call_github_api { |client, github_repo| client.close_issue(github_repo, number) }
   end
 
-  def self.list_issues(options = {})
-    call_github_api do |client, github_repo|
-      client.auto_paginate = true
-      client.list_issues(github_repo, **options)
-    end
-  end
-
   def self.add_label(label, color)
     call_github_api { |client, github_repo| client.add_label(github_repo, label, color) }
-  end
-
-  def self.get_label(name)
-    call_github_api { |client, github_repo| client.label(github_repo, name) }
   end
 
   def self.update_label(label, options)
@@ -42,16 +61,6 @@ class GithubHelper
 
   def self.delete_label(label)
     call_github_api { |client, github_repo| client.delete_label!(github_repo, label) }
-  end
-
-  def self.label_exists?(name)
-    labels = call_github_api { |client, github_repo| client.labels(github_repo) }
-    !labels.select { |label| label[:name] == name }.empty?
-  end
-
-  def self.issue_exists?(title)
-    issues = call_github_api { |client, github_repo| client.issues(github_repo) }
-    !issues.select { |issue| issue[:title] == title }.empty?
   end
 
   def self.call_github_api
