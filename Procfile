49f6bea4fec65f46ff2aaee9181c391281cadd89
Daisuke Satoh <daisuke.satoh@lifematics.co.jp>
2020-09-25 16:34:05 +0900

    update base image from strech to buster

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
-unicorn: bundle exec unicorn --env ${RAILS_ENV:-production} -c config/unicorn.rb
-sidekiq: bundle exec sidekiq -e production -C config/sidekiq.yml
+unicorn: unicorn --env ${RAILS_ENV:-production} -c config/unicorn.rb
+sidekiq: sidekiq -e production -C config/sidekiq.yml
--- docker/app/docker-entrypoint.sh
+++ docker/app/docker-entrypoint.sh
@@ -1,19 +1,22 @@
 #!/bin/bash
 
-if [[ $1 = "start" ]]; then
+if [[ $1 = start ]]; then
   mkdir -p /app/tmp/sockets /app/tmp/pids
   rm -f /app/tmp/sockets/* /app/tmp/pids/*
 
-  bundle install --path vendor/bundle
-
-  bundle exec rails assets:precompile
-
-  cp -rv /app/public/* /var/www/
+  if [[ $RAILS_ENV = production ]]; then
+    PROCFILE=${PROCFILE:-Procfile}
+    rails assets:precompile
+    mkdir -p /var/www
+    cp -rv /app/public/* /var/www/
+  else
+    PROCFILE=${PROCFILE:-Procfile.dev}
+  fi
 
   echo
   echo "start foreman..."
 
-  bundle exec foreman start
+  foreman start -f "$PROCFILE"
 else
   exec "$@"
 fi
